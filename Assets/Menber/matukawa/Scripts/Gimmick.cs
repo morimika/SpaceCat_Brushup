89d764103d40d4e05f23ed750f96724dbe4fd1dc
KAGAHARUHISA <ttc22c1020045@edu.tech.ac.jp>
2025-10-11 18:48:10 +0900

    加速ギミック作成中

---
+++ Assets/Menber/matukawa/Scripts/Gimmick.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class Gimmick : MonoBehaviour
+{
+    
+    public static float CatSpeed; // ‚Ë‚±‚Ì‘¬“x‚ðŽæ“¾
+    [SerializeField, Label("”L‚Ì‰Á‘¬“x(float)")] private float accelerateSpeed = 1.5f; // ”L‚É—^‚¦‚é‰Á‘¬“x
+    [Label("‰Á‘¬ƒMƒ~ƒbƒN")] public GameObject AccelerationGimmick;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
